a4d20badf07faed60553f569faf0250ec43b3d69
Anna <a.baranova@strategiedigitali.io>
2023-06-05 18:01:44 +0200

    STages!

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-console.log("Hello!");
+import { Color } from 'three';
+import { IfcViewerAPI } from 'web-ifc-viewer';
+
+
+const categoryFilter = document.getElementById("category-filter");
+
+const cards = document.querySelectorAll(".card");
+const cardsArray = Array.from(cards);
+
+const templateCard = cards[0];
+
+const categoryElement = (cards[0]).querySelector(".category");
+console.log(categoryElement);
+
+console.log(categoryElement.textContent);
+
+console.log(categoryFilter.value);
+
+
+categoryFilter.addEventListener("change", function () {
+  // Your code to filter the cards goes here
+  const cards = document.querySelectorAll(".card");
+    const selectedCategory = categoryFilter.value;
+
+    console.log(categoryFilter.value)
+    //console.log(categoryFilter.textContent)
+
+    cards.forEach(function (card) {
+      try{
+      card.classList.remove("hidden");
+      }
+      catch{}
+      if (
+        selectedCategory === "" ||
+        card.querySelector(".category").textContent == selectedCategory
+      ) {
+        card.classList.remove("hidden");
+      } else {
+        card.classList.add("hidden");
+      }
+    });
+});
+
+
+const container = document.getElementById('viewer-container');
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
+viewer.grid.setGrid();
+viewer.axes.setAxes();
+
+async function loadIfc(url) {
+    await viewer.IFC.setWasmPath("../../../");
+    const model = await viewer.IFC.loadIfcUrl(url);
+    viewer.shadowDropper.renderShadow(model.modelID);
+}
+
+loadIfc('your/IFC/path/model.ifc');
+
+const params = new URLSearchParams(window.location.search);
+  const modelName = params.get('model');
+  const modelNameElement = document.getElementById('model-name');
+  modelNameElement.textContent = modelName;
