d73ce674097d82ac110685f283bd998857751c86
Anna <a.baranova@strategiedigitali.io>
2023-06-06 10:47:39 +0200

    hmm

--- screen.js
+++ screen.js
@@ -1,7 +1,3 @@
-import { Color } from 'three';
-import { IfcViewerAPI } from 'web-ifc-viewer';
-
-
 const categoryFilter = document.getElementById("category-filter");
 
 const cards = document.querySelectorAll(".card");
@@ -41,20 +37,3 @@ categoryFilter.addEventListener("change", function () {
     });
 });
 
-
-const container = document.getElementById('viewer-container');
-const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
-viewer.grid.setGrid();
-viewer.axes.setAxes();
-
-async function loadIfc(url) {
-    const model = await viewer.IFC.loadIfcUrl(url);
-    viewer.shadowDropper.renderShadow(model.modelID);
-}
-
-loadIfc('./DR_2SL_Elevator Sliding Door.ifc');
-
-const params = new URLSearchParams(window.location.search);
-  const modelName = params.get('model');
-  const modelNameElement = document.getElementById('model-name');
-  modelNameElement.textContent = modelName;
