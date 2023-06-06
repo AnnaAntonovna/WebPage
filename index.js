8d2b062dd875e507b3a4a6a16795cf1e656e8c47
Anna <a.baranova@strategiedigitali.io>
2023-06-06 11:01:43 +0200

    Update index

--- index.js
+++ index.js
@@ -2,45 +2,6 @@ import { Color } from 'three';
 import { IfcViewerAPI } from 'web-ifc-viewer';
 
 
-const categoryFilter = document.getElementById("category-filter");
-
-const cards = document.querySelectorAll(".card");
-const cardsArray = Array.from(cards);
-
-const templateCard = cards[0];
-
-const categoryElement = (cards[0]).querySelector(".category");
-console.log(categoryElement);
-
-console.log(categoryElement.textContent);
-
-console.log(categoryFilter.value);
-
-
-categoryFilter.addEventListener("change", function () {
-  // Your code to filter the cards goes here
-  const cards = document.querySelectorAll(".card");
-    const selectedCategory = categoryFilter.value;
-
-    console.log(categoryFilter.value)
-    //console.log(categoryFilter.textContent)
-
-    cards.forEach(function (card) {
-      try{
-      card.classList.remove("hidden");
-      }
-      catch{}
-      if (
-        selectedCategory === "" ||
-        card.querySelector(".category").textContent == selectedCategory
-      ) {
-        card.classList.remove("hidden");
-      } else {
-        card.classList.add("hidden");
-      }
-    });
-});
-
 
 const container = document.getElementById('viewer-container');
 const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
